8d215a3e4d6451170d6cc3b1200e9b5033377f43
Lenny Kay <77846195+lenbot7@users.noreply.github.com>
2025-10-02 10:45:18 +0100

    turns out slopes are hell

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -18,8 +18,7 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if is_on_floor():
 		dashed_already = false
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -31,11 +30,15 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("dash") and not dashed_already:
 		velocity.x += direction * 500
 		dashed_already = true
+	if velocity.x < 0:
+		animated_sprite_2d.flip_h = true
+	elif velocity.x > 0:
+		animated_sprite_2d.flip_h = false
 	if direction:
 		velocity.x = move_toward(velocity.x, direction * SPEED, 50)
 		animated_sprite_2d.play("walk")
 	else:
-		velocity.x = move_toward(velocity.x, 0, 50)
+		velocity.x = move_toward(velocity.x, 0, 20)
 		animated_sprite_2d.play("default")
 	if Input.is_action_just_pressed("dash") and not dashed_already:
 		if direction:
